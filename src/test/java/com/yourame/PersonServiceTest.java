216d4d788f669939705a0c08ac2aa8992d38c06d
Errezzaki <demo65575@gmail.com>
2024-12-22 20:58:00 +0100

    make the second test

--- src/test/java/com/yourame/PersonServiceTest.java
+++ src/test/java/com/yourame/PersonServiceTest.java
@@ -26,4 +26,24 @@ public class PersonServiceTest {
                 assertThat(peopleLivingIn789StreetX).containsExactlyInAnyOrderElementsOf(expectedPersonsLivingIn789StreetX);
         }
 
+        @Test
+        public void testFilterAdults() {
+                List<Person> adults = PersonService.filterAdults();
+
+                // Liste attendue des adultes (18 ans ou plus)
+                List<Person> expectedAdultPersons = Arrays.asList(
+                                Person.builder().firstName("John").familyName("Doe")
+                                                .birthDate(LocalDate.of(1990, 5, 12))
+                                                .address("789 Street X").build(),
+                                Person.builder().firstName("Jane").familyName("Smith")
+                                                .birthDate(LocalDate.of(2005, 10, 15))
+                                                .address("101 Street Y").build(),
+                                Person.builder().firstName("Jim").familyName("Brown")
+                                                .birthDate(LocalDate.of(1985, 3, 9))
+                                                .address("789 Street X").build());
+
+                // Vérification avec AssertJ
+                assertThat(adults).containsExactlyInAnyOrderElementsOf(expectedAdultPersons);
+        }
+
 }
